94a673a887a5bfedc60048ba8edf77ae34f739a4
Sarobidy <mbolasaroubidy@gmail.com>
2024-10-28 14:33:53 +0100

    template-integrer

--- back/index.js
+++ back/index.js
@@ -9,11 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 var corsOptions = {
-    origin: ["http://localhost:5173","http://41.188.43.90:8082"]
+    origin: ["http://localhost:5173","http://localhost:3000","http://41.188.43.90:8082"]
 };
 app.use(cors(corsOptions));
 
 require("./controlleur/genre/genre.controlleur")(app);
 require("./controlleur/mpiangona/mpiangona.controlleur")(app);
+require("./controlleur/fiche/fiche.controlleur")(app);
 const httpServ = http.createServer(app);
 server.start(httpServ);
